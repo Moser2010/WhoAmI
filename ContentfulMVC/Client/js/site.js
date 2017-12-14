503985eb8c48bee789afdd18ea2981fd73302404
Moser2010 <cmoser351@gmail.com>
2017-12-14 10:43:38 -0600

    Update: Fixed Navbar on scroll

--- ContentfulMVC/Client/js/site.js
+++ ContentfulMVC/Client/js/site.js
@@ -16,7 +16,27 @@ var msnry = new Masonry('.grid', {
 //End: Masonry.js
 
 //Start:   Fixed on scroll Navbar
-window.addEventListener('scroll', function(e) {console.log("Scrolled");});
+var header = document.getElementsByTagName('header');
+var bar = document.getElementById("socialBar");
+var navLeft = document.getElementsByClassName("scrollNavLeft");
+var navCenter = document.getElementsByClassName("scrollNavCenter");
+header[0].addEventListener('click', function () {
+    header[0].className = "invisible";
+    console.log("Clicked on Header");
+})
+window.addEventListener('scroll', function (e) {
+    if (window.scrollY > header[0].offsetHeight + bar.offsetHeight / 2) {
+        bar.classList.add("fixed");
+        navLeft[0].classList.remove("invisible");
+        navCenter[0].classList.remove("invisible");
+    } else {
+        navLeft[0].classList.add("invisible");
+        navCenter[0].classList.add("invisible");
+
+        bar.classList.remove("fixed");
+  
+    }
+});
 
 
 //End:     Fixed on Scroll Navbar
