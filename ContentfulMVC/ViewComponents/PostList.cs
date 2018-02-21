5ca8efe77c89a9b766ec1bc1137df310266f0dd2
Moser2010 <cmoser351@gmail.com>
2018-02-21 14:15:32 -0600

    Converted partial to component.

---
+++ ContentfulMVC/ViewComponents/PostList.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contentful.Core;
+using Contentful.Core.Search;
+using ContentfulMVC.Models;
+
+namespace ContentfulMVC.ViewComponents
+{
+    public class PostList: ViewComponent
+    {
+        private readonly IContentfulClient _client;
+        public PostList(IContentfulClient client)
+        {
+            _client = client;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           
+            var posts = await _client.GetEntriesAsync<Posts>();
+            return View(posts);
+        }
+    }
+}
