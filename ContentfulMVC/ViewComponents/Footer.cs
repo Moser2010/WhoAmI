f9b8dc4cc09cb2236619b434cdb95705b6cc0b33
Moser2010 <cmoser351@gmail.com>
2018-02-21 12:48:02 -0600

    Moved Footer to a ViewComponent. Updated 12:45 PM Feb 21

---
+++ ContentfulMVC/ViewComponents/Footer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contentful.Core;
+using Contentful.Core.Search;
+using ContentfulMVC.Models;
+
+namespace ContentfulMVC.ViewComponents
+{
+    public class Footer: ViewComponent
+    {
+        private readonly IContentfulClient _client;
+        public Footer(IContentfulClient client)
+        {
+            _client = client;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //I dont fully understand the next two lines. I kinda understand what they are asking for but not fully!
+            var qb = QueryBuilder<MetaData>.New.FieldEquals("sys.id", "3A1FviHKNy6IQqSAq4UocQ");
+            var home = (await _client.GetEntriesAsync<MetaData>(qb)).FirstOrDefault();
+            return View(home);
+        }
+    }
+}
