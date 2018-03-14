51bbc6bbae0bb027d839b7cb730a2b94cf1f5323
Moser2010 <cmoser351@gmail.com>
2018-03-14 18:12:49 -0500

    Audio Visualizer

--- ContentfulMVC/Client/index.js
+++ ContentfulMVC/Client/index.js
@@ -1,3 +1,4 @@
-﻿import './scss/'
-import './js/site.js'
-import './js/markdown.js'
+﻿import './scss/';
+import './js/site.js';
+import './js/markdown.js';
+import './js/av.js';
---
+++ ContentfulMVC/Client/js/av.js
@@ -0,0 +1,21 @@
+﻿import "../../node_modules/p5/lib/p5";
+import "../../node_modules/p5/lib/addons/p5.dom"
+import "../../node_modules/p5/lib/addons/p5.sound";
+
+
+
+
+var song;
+function preload() {
+    console.log("We got here");
+    song = loadSound("https://chenyiya.com/codepen/deorro.mp3");
+}
+
+function setup() {
+    createCanvas(200, 200);
+    song.play();
+}
+
+function draw() {
+    background(0);
+}
