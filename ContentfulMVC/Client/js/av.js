aa006f7203d6ad8f363100b8d75dc247a3840604
Moser2010 <cmoser351@gmail.com>
2018-03-15 15:10:04 -0500

    Updated: 3/15 3:15

--- ContentfulMVC/Client/js/av.js
+++ ContentfulMVC/Client/js/av.js
@@ -1,28 +1,3 @@
 ﻿import "../../node_modules/p5/lib/p5";
 import "../../node_modules/p5/lib/addons/p5.dom";
 import "../../node_modules/p5/lib/addons/p5.sound";
-
-var canvas = document.getElementById("canvas");
-
-    console.log(canvas);
-    var song;
-    function preload() {
-        console.log("We got here");
-        song = loadSound("https://chenyiya.com/codepen/deorro.mp3");
-    }
-
-    function setup() {
-        createCanvas(200, 200);
-        song.play();
-    }
-
-    function draw() {
-        background(0);
-}
-
-if (canvas !== null) {
-    preload();
-    setup();
-    draw();
-}
-
