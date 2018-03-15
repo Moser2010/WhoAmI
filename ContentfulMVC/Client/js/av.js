2bd6d4b5128639f86a9ce994dee6ccc59ab24353
Moser2010 <cmoser351@gmail.com>
2018-03-15 18:22:05 -0500

    Updated: 6:15

--- ContentfulMVC/Client/js/av.js
+++ ContentfulMVC/Client/js/av.js
@@ -1,32 +1,32 @@
 ﻿
-import p5 from "p5/lib/p5";
+import p5 from "p5";
 import "p5/lib/addons/p5.dom";
 import "p5/lib/addons/p5.sound";
 
 var song;
 var amp;
+var width = window.innerWidth;
+var height = window.innerHeight
 
 
 
-
-var sketch = function (p5) {
+var sketch = function (p) {
     document.getElementById("audiofile").onchange = function (event) {
         if (event.target.files[0]) {
             // Load our new song
-            song = p5.loadSound(URL.createObjectURL(event.target.files[0]));
+            song = p.loadSound(URL.createObjectURL(event.target.files[0]));
         }
     };
     var gray = 0;
 
-    p5.setup = function () {
-        var width = window.innerWidth;
-        var height = window.innerHeight
-        var myCanvas = p5.createCanvas(width, height);
+    p.setup = function () {
+        
+        var myCanvas = p.createCanvas(width, height);
         myCanvas.parent("canvas");
     };
 
-    p5.draw = function () {
-        p5.background(50);
+    p.draw = function () {
+        p.background(50);
 
         if (typeof song !== "undefined" && song.isLoaded() && !song.isPlaying()) {
             // Do once
@@ -39,11 +39,12 @@ var sketch = function (p5) {
         if (typeof amp !== "undefined") {
             console.log("got in here")
             var vol = amp.getLevel();
-            var diam = p5.map(vol, 0, 0.3, 10, 200);
-            p5.fill(255, 0, 255);
-            p5.ellipse(width / 2, height / 2, diam, diam);
+            var diam = p.map(vol, 0, 0.3, 100, 500);
+            p.fill(255, 0, 255);
+            p.ellipse(width / 2, height / 2, diam, diam);
         }
     };
 };
 
 new p5(sketch);
+
