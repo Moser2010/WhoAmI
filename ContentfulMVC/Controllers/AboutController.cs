90341c42c0367f8373be0f3bad87a41ea4aac68e
Moser2010 <cmoser351@gmail.com>
2018-03-26 17:54:16 -0500

    added some new pages

---
+++ ContentfulMVC/Controllers/AboutController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ContentfulMVC.Models;
+using Contentful.Core;
+using Contentful.Core.Models;
+using Contentful.Core.Search;
+
+namespace ContentfulMVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IContentfulClient _client;
+        public AboutController(IContentfulClient client)
+        {
+            _client = client;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //var qb = QueryBuilder<Posts>.New.FieldEquals(f => f.sys.Id, id);
+            var allPosts = await _client.GetEntriesAsync<Posts>();
+            return View("Index", allPosts);
+        }
+        public async Task<IActionResult> WhoIAm()
+        {
+            var allPosts = await _client.GetEntriesAsync<Posts>();
+            return View("WhoIAm");
+        }
+        public async Task<IActionResult> WhatIDo()
+        {
+            var allPosts = await _client.GetEntriesAsync<Posts>();
+            return View("WhatIDo");
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
